08ef195b85e652cd49ee76e94858e9b272d33ca6
danilo <daniloglcomercial@gmail.com>
2023-08-10 14:03:50 -0300

    .

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -3,17 +3,49 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:loja_virtual/models/user_model.dart';
 
-class UserManager {
+import '../helpers/firebase_errors.dart';
+
+class UserManager extends ChangeNotifier {
+  UserManager() {
+    _loadCurrentUser();
+  }
+
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-  Future signIn(UserModel user) async {
+  User? currentUser;
+
+  bool _loading = false;
+  bool get loading => _loading;
+
+  Future signIn(
+      {required UserModel user,
+      required Function onFail,
+      required Function onSuccess}) async {
+    loading = true;
     try {
       final UserCredential result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
 
-      debugPrint(result.user!.uid);
+      currentUser = result.user;
+      onSuccess();
     } on PlatformException catch (e) {
-      debugPrint(e.toString());
+      onFail(getErrorString(e.code));
     }
+    loading = false;
+  }
+
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  Future<void> _loadCurrentUser() async {
+    auth.authStateChanges().listen((User? user) {
+      if (user != null) {
+        currentUser = user;
+        debugPrint(currentUser!.uid);
+      }
+      notifyListeners();
+    });
   }
 }
--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -24,66 +24,97 @@ class LoginScreen extends StatelessWidget {
         margin: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Form(
           key: formkey,
-          child: ListView(
-            padding: const EdgeInsets.all(16.0),
-            shrinkWrap: true,
-            children: [
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  hintText: "E-mail",
-                ),
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                validator: (email) {
-                  if (!emailValid(email.toString())) return 'E-mail invalido';
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                controller: passController,
-                decoration: const InputDecoration(
-                  hintText: "Senha",
-                ),
-                autocorrect: false,
-                obscureText: true,
-                validator: (pass) {
-                  if (pass!.isEmpty || pass.length < 6) return 'Senha invalida';
-                  return null;
-                },
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                    style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                    onPressed: () {},
-                    child: const Text('Esqueci minha senha')),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              SizedBox(
-                height: 44,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        textStyle: const TextStyle(color: Colors.white)),
-                    onPressed: () {
-                      if (formkey.currentState!.validate()) {
-                        context.read<UserManager>().signIn(UserModel(
-                            email: emailController.text,
-                            password: passController.text));
-                      }
+          child: Consumer<UserManager>(
+            builder: (context, userManager, child) {
+              return ListView(
+                padding: const EdgeInsets.all(16.0),
+                shrinkWrap: true,
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    enabled: !userManager.loading,
+                    decoration: const InputDecoration(
+                      hintText: "E-mail",
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    validator: (email) {
+                      if (!emailValid(email.toString()))
+                        return 'E-mail invalido';
+                      return null;
                     },
-                    child: const Text(
-                      "Entrar",
-                      style: TextStyle(fontSize: 18.0),
-                    )),
-              )
-            ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: passController,
+                    enabled: !userManager.loading,
+                    decoration: const InputDecoration(
+                      hintText: "Senha",
+                    ),
+                    autocorrect: false,
+                    obscureText: true,
+                    validator: (pass) {
+                      if (pass!.isEmpty || pass.length < 6)
+                        return 'Senha invalida';
+                      return null;
+                    },
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                        onPressed: () {},
+                        child: const Text('Esqueci minha senha')),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    height: 44,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(context).primaryColor,
+                            disabledBackgroundColor:
+                                Theme.of(context).primaryColor.withAlpha(100),
+                            textStyle: const TextStyle(color: Colors.white)),
+                        onPressed: userManager.loading
+                            ? null
+                            : () {
+                                if (formkey.currentState!.validate()) {
+                                  context.read<UserManager>().signIn(
+                                      user: UserModel(
+                                          email: emailController.text,
+                                          password: passController.text),
+                                      onFail: (e) {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(SnackBar(
+                                          content: Text('Falha ao Entrar $e'),
+                                          duration:
+                                              const Duration(milliseconds: 300),
+                                          backgroundColor: Colors.red,
+                                        ));
+                                      },
+                                      onSuccess: () {
+                                        debugPrint('sucessos');
+                                        //TODO: FECHAR TELA DE LOGIN
+                                      });
+                                }
+                              },
+                        child: userManager.loading
+                            ? const CircularProgressIndicator(
+                                valueColor:
+                                    AlwaysStoppedAnimation(Colors.white),
+                              )
+                            : const Text(
+                                "Entrar",
+                                style: TextStyle(fontSize: 18.0),
+                              )),
+                  )
+                ],
+              );
+            },
           ),
         ),
       )),
