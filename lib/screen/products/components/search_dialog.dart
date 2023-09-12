b9caf5ef1c3eb7a043fbcde79f8c69dafc4564cf
danilo <daniloglcomercial@gmail.com>
2023-09-12 15:56:24 -0300

    .

---
+++ lib/screen/products/components/search_dialog.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+
+class SearchDialog extends StatelessWidget {
+  const SearchDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          top: 2.0,
+          left: 2.0,
+          right: 2.0,
+          child: Card(
+            child: TextFormField(
+              textInputAction: TextInputAction.search,
+              autofocus: true,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(vertical: 15.0),
+                prefixIcon: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(Icons.arrow_back),
+                ),
+              ),
+              onFieldSubmitted: (text) {
+                Navigator.of(context).pop(text);
+              },
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
