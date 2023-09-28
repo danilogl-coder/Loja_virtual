5b8dda2549215a76d7687bf7845b16df2cb852ca
Danilo Guilherme <daniloglcomercial.com>
2023-09-27 21:57:12 -0300

    .

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,14 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_virtual/models/item_size.dart';
 
 class Product {
   Product.fromDocument(DocumentSnapshot document) {
     id = document.id;
     name = document['name'] as String;
     description = document['description'] as String;
-    images = List<String>.from(document.get('images') as List<dynamic>);
+    images = List<String>.from(document.get('images') as List<dynamic> ?? []);
+    sizes= (document.get('sizes') as List<dynamic>).map((s) => ItemSize.fromMap(s as Map<String, dynamic>)).toList();
+
+
+    print(sizes);
   }
   String? id;
   String? name;
   String? description;
   List<String>? images;
+  List<ItemSize>? sizes;
 }
