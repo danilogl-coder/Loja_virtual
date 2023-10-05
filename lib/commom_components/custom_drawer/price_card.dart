cefa427fe9fecea38632322a9790cdec673f7aa5
Danilo <daniloglcomercial@gmail.com>
2023-10-05 20:49:29 -0300

    .

---
+++ lib/commom_components/custom_drawer/price_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/cart_manager.dart';
+import 'package:provider/provider.dart';
+
+class PriceCard extends StatelessWidget {
+  const PriceCard({super.key, required this.buttonText, required this.onPressed});
+
+  final String buttonText;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final cartManage = context.watch<CartManager>();
+    
+    return  Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Padding(padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 4.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text('Resumo do Pedido', textAlign: TextAlign.start,style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16.0),),
+         const SizedBox(height: 12),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Subtotal'),
+              Text('R\$ 19.99'),
+            ],
+          ),
+          const Divider(),
+          const SizedBox(height: 12.0,),
+          Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Total', style: TextStyle(fontWeight: FontWeight.w600),),
+              Text('R\$ 19,99', style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 16.0),)
+            ],
+          ),
+          const SizedBox(height: 8.0,),
+         ElevatedButton(
+          onPressed: onPressed, style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Theme.of(context).primaryColor)), child:  Text(buttonText, style: const TextStyle(color: Colors.white),)) 
+        ],
+      ) ,),
+    );
+  }
+}
